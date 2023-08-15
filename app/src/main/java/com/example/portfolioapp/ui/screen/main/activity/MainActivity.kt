497019efcd611d2caf48aa063b14365bfbd29dae
Marcelo Silveira <marcelojunior543@gmail.com>
2023-08-15 16:52:07 -0300

    first_configs - feat: Implemented BottomNavigationView.

--- app/src/main/java/com/example/portfolioapp/ui/screen/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/portfolioapp/ui/screen/main/activity/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.portfolioapp.ui.screen.main.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupWithNavController
 import com.example.portfolioapp.R
+import com.example.portfolioapp.databinding.ActivityMainBinding
+import com.example.portfolioapp.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate) {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun ActivityMainBinding.initialize(savedInstanceState: Bundle?) {
+        setupBottomNavigationView()
+    }
+
+    private fun setupBottomNavigationView() {
+        val navHostFragment = supportFragmentManager.findFragmentById(
+            R.id.nav_host_container
+        ) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigation.apply {
+            itemIconTintList = null
+            setupWithNavController(navController)
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
