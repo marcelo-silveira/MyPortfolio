337265fede4a4aee9631af1260ac71c30189ee54
Marcelo Silveira <marcelojunior543@gmail.com>
2023-08-15 15:23:08 -0300

    first_configs - feat: Implemented navigation components for BottomNavigationView.

---
+++ app/src/main/java/com/example/portfolioapp/ui/screen/main/menu/view/MenuFragment.kt
@@ -0,0 +1,8 @@
+package com.example.portfolioapp.ui.screen.main.menu.view
+
+import com.example.portfolioapp.databinding.FragmentMenuBinding
+import com.example.portfolioapp.ui.base.BaseFragment
+
+class MenuFragment : BaseFragment<FragmentMenuBinding>(FragmentMenuBinding::inflate) {
+
+}
---
+++ app/src/main/java/com/example/portfolioapp/ui/screen/main/monster/view/MonsterFragment.kt
@@ -0,0 +1,8 @@
+package com.example.portfolioapp.ui.screen.main.monster.view
+
+import com.example.portfolioapp.databinding.FragmentMonsterBinding
+import com.example.portfolioapp.ui.base.BaseFragment
+
+class MonsterFragment : BaseFragment<FragmentMonsterBinding>(FragmentMonsterBinding::inflate) {
+
+}
